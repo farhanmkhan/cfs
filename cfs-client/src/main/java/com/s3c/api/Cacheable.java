b7834f64199be29b6495dd0e06cecf91d13440c5
Farhan Khan <farhan_m_khan@yahoo.com>
2017-06-20 23:05:38 -0500

    Adding Spring Boot

--- cfs-client/src/main/java/com/s3c/api/Cacheable.java
+++
@@ -1,8 +0,0 @@
-package com.s3c.api;
-
-/**
- * Created by fkhan on 6/3/17.
- */
-public interface Cacheable {
-
-}
---
+++ cfs-server/src/main/java/cfs/server/Application.java
@@ -0,0 +1,15 @@
+package cfs.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by farhankhan on 6/20/17.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
